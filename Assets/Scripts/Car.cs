d4d8958de868b0ba36deab1c563e8b7ebd371d9d
reza rezaee <rezarezaee.commercial@gmail.com>
2023-01-04 11:46:03 +0330

    add lifetime for player healthness

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour {
+	public int lifetime=3;
+
 	void Start () {
 
 	}
@@ -14,6 +16,10 @@ public class Car : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.tag=="Wheel"){
+			lifetime--;
+			print("Hey you lose a lifetime, and have " + lifetime + " lifetime");
+		}
+		if(lifetime==0){
 			Time.timeScale = 0;
 		}
 	}
